6ddea5236275dd4c0d57e5d75276391a8ad0211a
Nestor Antonio Segura González <nsegurag@miumg.edu.gt>
2024-10-13 12:31:56 -0600

    Proyecto UMG

---
+++ Factura/src/main/java/com/factura/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.factura.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = "Error de validación: " + ex.getBindingResult().getFieldError().getDefaultMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+}
